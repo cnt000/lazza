944002331905a3c78645e38ef91055c07ba5c0f9
cnt000 <cnt000@gmail.com>
2016-12-11 21:34:44 +0100

    json into array in php

--- server/savefinal.php
+++ server/savefinal.php
@@ -1,26 +1,61 @@
 <?php
 
-  // $dbtype     = "mysql";
-  // $dbhost     = "";
-  // $dbname     = "";
-  // $dbuser     = "";
-  // $dbpass     = "";
-  // $table = 'lazzaroni';
+  include_once("db.inc.php");
 
-  // $conn = new SafePDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
+  $conn = new SafePDO("mysql:host=$dbhost;dbname=$dbname",$dbuser,$dbpass);
 
-  // $query = "insert into $table_comments ($fields_comment) values (:type, :name, :email, :comment, NOW())";
-  //
-  // $query->bindParam(':name', $name, PDO::PARAM_STR, 50);
-  // $query->bindParam(':email', $email, PDO::PARAM_STR, 50);
-  //
-  // $query->execute();
+  $query_str = "insert into $table values (:session, :judge, :data, NOW())";
+
+  $query = $conn->prepare($query_str);
 
   $request_body = file_get_contents('php://input');
-  $data = json_encode($request_body);
-  // $results = array("result" => $_POST['judging']);
-  // $result = json_encode($results);
-  header('Content-type: application/json');
-  echo urldecode($data);
+  $data = json_encode($request_body, true);
+  $json = urldecode($data);
+  $new_json = str_replace('json=', "", $json);
+  $asd = trim($new_json, "\"");
+  $manage = json_decode($asd, true);
+
+  $judge = "";
+  foreach ($manage['fields'] as $key => $value) {
+    if($value['id'] == "judge") {
+      $judge = $value['value'];
+    }
+  }
+  $session = $manage['session'];
+  $query->bindParam(':session', $session, PDO::PARAM_STR, 100);
+  $query->bindParam(':judge', $session, PDO::PARAM_STR, 100);
+  $query->bindParam(':data', $new_json, PDO::PARAM_STR, 5000);
+
+  $query->execute();
+
+  echo $session;
+  echo $judge;
+  print_r($asd);
+  //header('Content-type: application/json');
+
+
+
+
+  class SafePDO extends PDO {
+
+  	public static function exception_handler($exception) {
+  		// Output the exception details
+  		print_r($exception->getMessage());
+  		print('Uncaught exception: ');
+  	}
+
+  	public function __construct($dsn, $username='', $password='', $driver_options=array()) {
+
+  		// Temporarily change the PHP exception handler while we . . .
+  		set_exception_handler(array(__CLASS__, 'exception_handler'));
+
+  		// . . . create a PDO object
+  		parent::__construct($dsn, $username, $password, $driver_options);
+
+  		// Change the exception handler back to whatever it was before
+  		restore_exception_handler();
+  	}
+
+  }
 
  ?>
