b704b11e45bea7f653bb7a606d1b1985038206a4
cnt000 <cnt000@gmail.com>
2017-02-03 21:28:37 +0100

    call to teams.json

--- src/actions/index.js
+++ src/actions/index.js
@@ -47,9 +47,9 @@ export const vote = (id, value, oneshot) => {
 }
 
 function fetchTeams(subreddit) {
-  return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+  return fetch(`teams.json`)
   .then(response => response.json())
-  .then(json => json.data.children.map(child => child.data))
+
 }
 
 function saveResults(state) {
