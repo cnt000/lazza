88bb091911efa2838357808be85d3a1e679b4154
cnt000 <cnt000@gmail.com>
2017-02-16 19:52:15 +0100

    server test

--- server/index.js
+++ server/index.js
@@ -1,1 +1,21 @@
-console.log('ciao');
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;        // set our port
+
+var router = express.Router();              // get an instance of the express Router
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Magic happens on port ' + port);
